32f595b81dc4122322c4a528d3ae5a5e5061580a
Matt Hicks <matt@outr.com>
2013-10-04 17:02:45 -0500

    Began work on creating Communicator jQuery plugin with GWT.

--- communicator-client/src/main/java/com/outr/citonet/communicator/client/Communicator.java
+++ communicator-client/src/main/java/com/outr/citonet/communicator/client/Communicator.java
@@ -9,9 +9,40 @@ public class Communicator implements EntryPoint {
     @Override
     public void onModuleLoad() {
         log("Communicator loaded!");
+        initializeJQueryPlugin();
     }
 
     public static native void log(String message) /*-{
         console.log(message);
+    }-*/;
+
+    private native void initializeJQueryPlugin() /*-{
+        (function($) {
+            var dataKey = 'communicator_settings';
+            var methods = {
+                init: function(options) {
+                    var settings = $.extend({
+                        id: null,
+                        webSocketURL: null,
+                        pollingURL: null,
+                        sendingURL: null,
+                        timeout: 30000
+                    }, options);
+                    return this.each(function() {
+                        var thisElement = this;
+                        var $this = $(this);
+                        $this.data(dataKey, settings);
+                        $.each(settings, function(key, value) {
+                            methods.updated.apply(thisElement, [key, value]);
+                        });
+                    });
+                },
+                option: function(key, value) {
+                    if (value) {
+
+                    }
+                }
+            }
+        })($wnd.jQuery);
     }-*/;
 }

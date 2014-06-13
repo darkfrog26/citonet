f4122c1110196d003cb7417758da6efd435217d2
Matt Hicks <matt@outr.com>
2014-06-12 21:06:45 -0500

    Additional features added to MimeType.

--- core/src/main/scala/com/outr/net/http/mime/MimeType.scala
+++ core/src/main/scala/com/outr/net/http/mime/MimeType.scala
@@ -1,6 +1,8 @@
 package com.outr.net.http.mime
 
 import scala.io.Source
+import java.io.File
+import java.nio.file.Files
 
 /**
  * @author Matt Hicks <matt@outr.com>
@@ -24,6 +26,14 @@ object MimeType {
   } else {
     default
   }
+
+  def fromFilename(filename: String, default: String): Option[String] = if (filename.indexOf('.') != -1) {
+    Option(lookup(filename.substring(filename.lastIndexOf('.') + 1), default))
+  } else {
+    None
+  }
+
+  def fromFile(file: File): Option[String] = fromFilename(file.getName, Files.probeContentType(file.toPath))
 
   // Load mime types from file
   private def load() = {

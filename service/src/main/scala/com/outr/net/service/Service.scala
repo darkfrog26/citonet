64b34b0c30ebf3913d3a4531d3a201743971aa48
Matt Hicks <matt@outr.com>
2015-01-26 14:50:23 -0600

    Converted Service to be instantiatable on its own or via companion. Added 'pretty' property to define how the Service should communicate. Switched Service to use typed conversion from JSON to avoid class or type information being required. Added bind / unbind support properly to Service.

--- service/src/main/scala/com/outr/net/service/Service.scala
+++ service/src/main/scala/com/outr/net/service/Service.scala
@@ -1,6 +1,7 @@
 package com.outr.net.service
 
 import com.outr.net.http.content.{ContentType, StringContent}
+import com.outr.net.http.handler.PathMappingHandler
 import com.outr.net.http.session.Session
 import com.outr.net.http.{WebApplication, HttpHandler}
 import com.outr.net.http.request.HttpRequest
@@ -8,27 +9,28 @@ import com.outr.net.http.response.{HttpResponseStatus, HttpResponse}
 
 import org.powerscala.json._
 import org.powerscala.log.Logging
+import org.powerscala.property.Property
 
 /**
  * @author Matt Hicks <matt@outr.com>
  */
-class Service[In, Out] private(f: In => Out)(implicit inManifest: Manifest[In], outManifest: Manifest[Out]) extends HttpHandler with Logging {
+abstract class Service[In <: AnyRef, Out](implicit inManifest: Manifest[In], outManifest: Manifest[Out]) extends HttpHandler with Logging {
+  val pretty = Property[Boolean](default = Some(false))
+
+  def apply(request: In): Out
+
   override def onReceive(request: HttpRequest, response: HttpResponse) = {
     val json = requestJSON(request)
     json match {
       case Some(content) => {
-        val in = fromJSON(content)
-        if (!in.getClass.isAssignableFrom(inManifest.runtimeClass)) {
-          response.copy(content = StringContent(s"Unable to convert $content to ${inManifest.runtimeClass.getName}. Converted status: $in"), status = HttpResponseStatus.BadRequest)
-        } else {
-          try {
-            val out = f(in.asInstanceOf[In])
-            response.copy(content = StringContent(toJSON(out).pretty, ContentType.JSON), status = HttpResponseStatus.OK)
-          } catch {
-            case t: Throwable => {
-              warn(s"Error occurred in handler function with input of $in.", t)
-              response.copy(content = StringContent(Service.InternalError), status = HttpResponseStatus.InternalServerError)
-            }
+        val in = typedJSON[In](content)
+        try {
+          val out = apply(in.asInstanceOf[In])
+          response.copy(content = StringContent(toJSON(out).stringify(pretty = pretty()), ContentType.JSON), status = HttpResponseStatus.OK)
+        } catch {
+          case t: Throwable => {
+            warn(s"Error occurred in handler function with input of $in.", t)
+            response.copy(content = StringContent(Service.InternalError), status = HttpResponseStatus.InternalServerError)
           }
         }
       }
@@ -46,13 +48,22 @@ class Service[In, Out] private(f: In => Out)(implicit inManifest: Manifest[In], 
         key -> values.head
       }
     }
-    Some(toJSON(map).pretty)
+    val json = toJSON(map)
+    Some(json.stringify(pretty = pretty()))
   } else {
     None
   }
 
   def bindTo[S <: Session](application: WebApplication[S], uris: String*) = {
-    application.addHandler(this, uris: _*)
+    uris.foreach {
+      case uri => PathMappingHandler.add(application, uri, this)
+    }
+  }
+
+  def unbindFrom[S <: Session](application: WebApplication[S], uris: String*) = {
+    uris.foreach {
+      case uri => PathMappingHandler.remove(application, uri)
+    }
   }
 }
 
@@ -60,7 +71,9 @@ object Service {
   private val EmptyResponseMessage = "No content in request."
   private val InternalError = "An error occurred processing the service request."
 
-  def apply[In, Out](f: In => Out)(implicit manifest: Manifest[In], outManifest: Manifest[Out]) = {
-    new Service[In, Out](f)
+  def apply[In <: AnyRef, Out](f: In => Out)(implicit manifest: Manifest[In], outManifest: Manifest[Out]) = {
+    new Service[In, Out] {
+      override def apply(request: In) = f(request)
+    }
   }
 }
--- service/src/test/scala/com/outr/net/service/ServiceSpec.scala
+++ service/src/test/scala/com/outr/net/service/ServiceSpec.scala
@@ -1,10 +1,13 @@
 package com.outr.net.service
 
-import com.outr.net.URL
+import com.outr.net.{Method, URL}
 import com.outr.net.http.WebApplication
+import com.outr.net.http.content.{StringContent, ContentType}
 import com.outr.net.http.request.HttpRequest
 import com.outr.net.http.response.{HttpResponseStatus, HttpResponse}
 import com.outr.net.http.session.MapSession
+import org.json4s.JsonAST.JString
+import org.powerscala.json.MapSupport
 import org.scalatest.{Matchers, WordSpec}
 
 /**
@@ -13,23 +16,26 @@ import org.scalatest.{Matchers, WordSpec}
 class ServiceSpec extends WordSpec with Matchers {
   "TestService" should {
     "configure the service" in {
-      val service = Service[Receiving, Sending] {
-        case receiving => {
-          println(s"Receiving: $receiving")
-          Sending(receiving.name.reverse)
-        }
-      }
-      service.bindTo(TestApplication, "/service")
+      ReverseService.bindTo(TestApplication, "/service")
     }
     "call the service with GET parameters" in {
       val response = TestApplication.onReceive(HttpRequest(url = URL("http://localhost/service?name=Hello")), HttpResponse(status = HttpResponseStatus.NotFound))
-      println(response.content.asString)
+      response.status should equal(HttpResponseStatus.OK)
+      response.content.contentType should equal(ContentType.JSON)
+      response.content.asString should equal("""{"name":"olleH"}""")
+    }
+    "call the service with POST JSON" in {
+      val content = StringContent("""{"name":"Hello"}""", ContentType.JSON)
+      val response = TestApplication.onReceive(HttpRequest(url = URL("http://localhost/service"), method = Method.Post, content = Some(content)), HttpResponse(status = HttpResponseStatus.NotFound))
+      response.status should equal(HttpResponseStatus.OK)
+      response.content.contentType should equal(ContentType.JSON)
+      response.content.asString should equal("""{"name":"olleH"}""")
     }
     "unbind the service" in {
-
+      ReverseService.unbindFrom(TestApplication, "/service")
     }
     "verify the service is no longer receiving" in {
-
+      TestApplication.onReceive(HttpRequest(url = URL("http://localhost/service?name=Hello")), HttpResponse(status = HttpResponseStatus.NotFound)).status should equal(HttpResponseStatus.NotFound)
     }
   }
 }
@@ -38,6 +44,12 @@ object TestApplication extends WebApplication[MapSession] {
   override protected def init() = {}
 
   override protected def createSession(request: HttpRequest, id: String) = new MapSession(id, this)
+}
+
+object ReverseService extends Service[Receiving, Sending] {
+  MapSupport.o2j.removeWhen("class", JString("com.outr.net.service.Sending"))
+
+  override def apply(receiving: Receiving) = Sending(receiving.name.reverse)
 }
 
 case class Receiving(name: String)

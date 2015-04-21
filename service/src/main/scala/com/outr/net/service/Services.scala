a4fa2165a4a8166ceb683a98e86a1cd47be7d2b9
Matt Hicks <matt@outr.com>
2015-04-21 13:17:36 -0500

    Added convenience method for regex matching services.

--- service/src/main/scala/com/outr/net/service/Services.scala
+++ service/src/main/scala/com/outr/net/service/Services.scala
@@ -1,7 +1,8 @@
 package com.outr.net.service
 
 import com.outr.net.http.WebApplication
-import com.outr.net.http.session.Session
+
+import scala.util.matching.Regex
 
 /**
  * @author Matt Hicks <matt@outr.com>
@@ -14,6 +15,12 @@ trait Services {
     val s = Service(f)(manifest, outManifest)
     val p = if (path.endsWith("/")) path else s"$path/"
     s.bindTo(application, s"${p}${name}")
+    s
+  }
+
+  protected def service[In, Out](regex: Regex)(f: In => Out)(implicit inManifest: Manifest[In], outManifest: Manifest[Out]) = {
+    val s = Service(f)(manifest, outManifest)
+    application.addHandler(s, regex)
     s
   }
 }

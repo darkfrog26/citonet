8c43e642a77adae84c299f8449983097b7b5ac6f
Matt Hicks <matt@outr.com>
2015-05-06 11:20:00 -0500

    Modified parsing support for IPv6 to support hexadecimal and scope id.

--- core/src/main/scala/com/outr/net/IP.scala
+++ core/src/main/scala/com/outr/net/IP.scala
@@ -14,11 +14,11 @@ object IP {
   lazy val LocalHost = IPv4()
 
   val IPv4Regex = """\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b""".r
-  val IPv6Regex = """(\d*):(\d*):(\d*):(\d*):(\d*):(\d*):(\d*):(\d*)""".r
+  val IPv6Regex = """([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*):([0-9a-fA-F]*)%?(\d*)""".r
 
   def get(address: String) = address match {
     case IPv4Regex(p1, p2, p3, p4) => Some(IPv4(p1.toInt, p2.toInt, p3.toInt, p4.toInt))
-    case IPv6Regex(p1, p2, p3, p4, p5, p6, p7, p8) => Some(IPv6(p1.toInt, p2.toInt, p3.toInt, p4.toInt, p5.toInt, p6.toInt, p7.toInt, p8.toInt))
+    case IPv6Regex(p1, p2, p3, p4, p5, p6, p7, p8, scope) => Some(IPv6(Some(p1), Some(p2), Some(p3), Some(p4), Some(p5), Some(p6), Some(p7), Some(p8), Option(scope)))
     case _ => None
   }
 
--- core/src/main/scala/com/outr/net/IPv6.scala
+++ core/src/main/scala/com/outr/net/IPv6.scala
@@ -3,7 +3,24 @@ package com.outr.net
 /**
  * @author Matt Hicks <matt@outr.com>
  */
-case class IPv6(part1: Int = 0, part2: Int = 0, part3: Int = 0, part4: Int = 0, part5: Int = 0, part6: Int = 0, part7: Int = 0, part8: Int = 1) extends IP {
-  lazy val address = Array(part1, part2, part3, part4, part5, part6, part7, part8)
-  lazy val addressString = s"$part1:$part2:$part3:$part4:$part5:$part6:$part7:$part8"
+case class IPv6(part1: Option[String] = None, part2: Option[String] = None, part3: Option[String] = None, part4: Option[String] = None, part5: Option[String] = None, part6: Option[String] = None, part7: Option[String] = None, part8: Option[String] = Some("1"), scope: Option[String] = None) extends IP {
+  private def s(part: Option[String]) = part.getOrElse("0")
+  def p1 = s(part1)
+  def p2 = s(part2)
+  def p3 = s(part3)
+  def p4 = s(part4)
+  def p5 = s(part5)
+  def p6 = s(part6)
+  def p7 = s(part7)
+  def p8 = s(part8)
+
+  def toInt(part: Option[String]) = Integer.parseInt(part.getOrElse("0"), 16)
+
+  lazy val address = Array(toInt(part1), toInt(part2), toInt(part3), toInt(part4), toInt(part5), toInt(part6), toInt(part7), toInt(part8))
+  lazy val addressString = s"$p1:$p2:$p3:$p4:$p5:$p6:$p7:$p8${scope.map(s => s"%$s").getOrElse("")}"
+
+  override def equals(o: scala.Any) = o match {
+    case ip: IPv6 => addressString == ip.addressString
+    case _ => false
+  }
 }

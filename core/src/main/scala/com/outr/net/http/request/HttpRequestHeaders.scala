2e0ea98f9ca902dfaf16e2fc6b7bfe90c1084c0e
Matt Hicks <matt@outr.com>
2015-05-16 10:36:27 -0500

    Minor fix for coner-case in cookie parsing.

--- core/src/main/scala/com/outr/net/http/request/HttpRequestHeaders.scala
+++ core/src/main/scala/com/outr/net/http/request/HttpRequestHeaders.scala
@@ -20,12 +20,16 @@ case class HttpRequestHeaders(values: Map[String, String]) extends HttpHeaders w
 
   private def parseCookie(s: String) = {
     val splitPoint = s.indexOf('=')
-    val name = s.substring(0, splitPoint).trim
-    val value = s.substring(splitPoint + 1).trim match {
-      case v if v.startsWith("\"") && v.endsWith("\"") => unescape(v.substring(1, v.length - 1))
-      case v => v
+    if (splitPoint > 0) {
+      val name = s.substring(0, splitPoint).trim
+      val value = s.substring(splitPoint + 1).trim match {
+        case v if v.startsWith("\"") && v.endsWith("\"") => unescape(v.substring(1, v.length - 1))
+        case v => v
+      }
+      name -> Cookie(name = name, value = value)
+    } else {
+      s -> Cookie(name = s, value = "")
     }
-    name -> Cookie(name = name, value = value)
   }
 
   private def unescape(s: String) = {   // TODO: make this work a lot better

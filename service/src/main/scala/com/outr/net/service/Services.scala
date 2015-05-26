ecbe9b824bb32a835ab221830b7cc71423bbb4a6
Matt Hicks <matt@outr.com>
2015-05-26 12:53:36 -0500

    Updates to Services to allow custom binders, auto-excluding of classes in generated output, and manual exclusion of class names.

--- service/src/main/scala/com/outr/net/service/Services.scala
+++ service/src/main/scala/com/outr/net/service/Services.scala
@@ -1,7 +1,10 @@
 package com.outr.net.service
 
 import com.outr.net.http.WebApplication
+import org.json4s.JsonAST.JString
+import org.powerscala.json.MapSupport
 
+import scala.language.implicitConversions
 import scala.util.matching.Regex
 
 /**
@@ -11,16 +14,32 @@ trait Services {
   def application: WebApplication
   def path: String
 
-  protected def service[In, Out](name: String)(f: In => Out)(implicit manifest: Manifest[In], outManifest: Manifest[Out]) = {
-    val s = Service(f)(manifest, outManifest)
-    val p = if (path.endsWith("/")) path else s"$path/"
-    s.bindTo(application, s"${p}${name}")
-    s
-  }
+  implicit def string2Binder(name: String): ServiceBinder = NamedServiceBinder(name)
+  implicit def regex2Binder(regex: Regex): ServiceBinder = RegexServiceBinder(regex)
 
-  protected def service[In, Out](regex: Regex)(f: In => Out)(implicit inManifest: Manifest[In], outManifest: Manifest[Out]) = {
-    val s = Service(f)(manifest, outManifest)
-    application.addHandler(s, regex)
-    s
+  def excludeClassFromOutput[C](implicit manifest: Manifest[C]) = MapSupport.o2j.removeWhen("class", JString(manifest.runtimeClass.getName))
+
+  protected def service[In, Out](binder: ServiceBinder, excludeClass: Boolean = true)(f: In => Out)(implicit manifest: Manifest[In], outManifest: Manifest[Out]) = {
+    if (excludeClass) excludeClassFromOutput[Out]
+    val service = Service(f)(manifest, outManifest)
+    binder.bindTo(application, path, service)
+    service
+  }
+}
+
+trait ServiceBinder {
+  def bindTo[In, Out](application: WebApplication, path: String, service: Service[In, Out]): Unit
+}
+
+case class NamedServiceBinder(name: String) extends ServiceBinder {
+  override def bindTo[In, Out](application: WebApplication, path: String, service: Service[In, Out]) = {
+    val p = if (path.endsWith("/")) path else s"$path/"
+    service.bindTo(application, s"${p}${name}")
+  }
+}
+
+case class RegexServiceBinder(regex: Regex) extends ServiceBinder {
+  override def bindTo[In, Out](application: WebApplication, path: String, service: Service[In, Out]) = {
+    application.addHandler(service, regex)
   }
 }
